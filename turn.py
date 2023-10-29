618fa5bfe12a3d5478b9a267652672ccb174d288
joshita148 <69581254+joshita148@users.noreply.github.com>
2023-10-29 20:44:37 +0530

    Update turn.py

--- turn.py
+++ turn.py
@@ -13,7 +13,7 @@ def get_ice_servers():
 
     try:
         account_sid = "AC25b01fca5e4873e773f706152764fd29"
-        auth_token = "f0189d9d93e956d18c883705a14d69a1"
+        auth_token = "331fd76f4e1f6ab4e836bab7b71690cf"
     except KeyError:
         logger.warning(
             "Twilio credentials are not set. Fallback to a free STUN server from Google."  # noqa: E501
